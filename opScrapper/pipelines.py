f832638e42e3fc04277f3e71223a8f85ae857cf8
Rohit Bhetal <96344296+Rohit-Bhetal@users.noreply.github.com>
2024-11-14 22:14:34 +0530

    Inital

--- opScrapper/pipelines.py
+++ opScrapper/pipelines.py
@@ -5,6 +5,7 @@
 
 
 # useful for handling different item types with a single interface
+import json
 import firebase_admin
 import firebase_admin.db
 from itemadapter import ItemAdapter
@@ -17,8 +18,9 @@ class OpscrapperPipeline:
     def __init__(self):
         #service=Path(__file__).parent
         #service_path_json= service/'manga-scrapy-firebase-adminsdk-p41zm-04853ada62.json'
-        service_path_json=dict(os.getenv("FIREBASE_CREDENTIALS_PATH"))
-        cred=firebase_admin.credentials.Certificate(service_path_json)
+        service_path_json=os.getenv("FIREBASE_CREDENTIALS_PATH")
+        firebase_cred_dict = json.loads(service_path_json)
+        cred=firebase_admin.credentials.Certificate(firebase_cred_dict)
         firebase_admin.initialize_app(cred, {
             'databaseURL': os.getenv("FIREBASE_DB_URI")
         })
