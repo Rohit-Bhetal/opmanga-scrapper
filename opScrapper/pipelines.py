8589bdee2c4317b752707e6829dde2fa83ae69f9
Rohit Bhetal <96344296+Rohit-Bhetal@users.noreply.github.com>
2024-11-14 17:42:58 +0530

    Inital

--- opScrapper/pipelines.py
+++ opScrapper/pipelines.py
@@ -10,13 +10,14 @@ import firebase_admin.db
 from itemadapter import ItemAdapter
 from pathlib import Path
 from scrapy.exceptions import DropItem
-
+import os
 
 class OpscrapperPipeline:
     
     def __init__(self):
-        service=Path(__file__).parent
-        service_path_json= service/'manga-scrapy-firebase-adminsdk-p41zm-04853ada62.json'
+        #service=Path(__file__).parent
+        #service_path_json= service/'manga-scrapy-firebase-adminsdk-p41zm-04853ada62.json'
+        service_path_json=os.getenv("FIREBASE_CREDENTIALS_PATH")
         cred=firebase_admin.credentials.Certificate(service_path_json)
         firebase_admin.initialize_app(cred, {
             'databaseURL': 'https://manga-scrapy-default-rtdb.asia-southeast1.firebasedatabase.app/'
